4602388fd0a72dac6a6001acc9747be7598747f3
dah <cdh010126r@gmail.com>
2024-09-09 19:17:03 +0900

    feat: keyframe animation

--- src/components/home/about-me.js
+++ src/components/home/about-me.js
@@ -1,52 +1,52 @@
 import Image from "next/image";
 import { motion } from "framer-motion";
-import ProfileImg from "${prefix}/assets/img/채다희_23.11.jpg";
-import { useEffect, useState } from "react";
 
 export default function AboutMe() {
-  const fullText = `꼼꼼꼼하고 차분하게 생각하는 것을 목표로 하는\nFrontEnd  개발자 채다희입니다.`;
-  const [displayedText, setDisplayedText] = useState("");
-
-  useEffect(() => {
-    let index = 0;
-    const interval = setInterval(() => {
-      if (index < fullText.length) {
-        setDisplayedText((prev) => prev + fullText.charAt(index));
-        index++;
-      } else {
-        clearInterval(interval);
-      }
-    }, 50); // 타이핑 속도 조절
-    return () => clearInterval(interval);
-  }, [fullText]);
+  const prefix =
+    process.env.NODE_ENV === "production"
+      ? "https://chae-dahee.github.io/"
+      : "";
+  const profileImgSrc = `${prefix}/채다희_23.11.jpg`;
 
   return (
     <section className="flex flex-col items-center justify-center min-h-screen text-gray-600 body-font">
       <div className="container flex flex-col items-start px-5 pt-5 pb-5 mx-auto md:flex-row">
         <div className="w-5/6 mb-10 lg:max-w-lg lg:w-full d:mb-0">
           <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            transition={{ duration: 2 }}
+            initial={{ opacity: 0, y: 5 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: 5 }}
+            transition={{ duration: 4 }}
           >
             <Image
-              src={ProfileImg}
+              src={profileImgSrc}
               alt="채다희"
               width={300}
+              height={500}
               className="mx-auto"
-            />{" "}
+            />
           </motion.div>
         </div>
-        <div className="flex flex-col items-center text-center lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 md:items-start md:text-left">
-          <motion.h1 className="mb-4 text-lg font-medium text-gray-900 w-[89%] title-font sm:text-4xl sm:leading-relaxed">
-            {displayedText}
-          </motion.h1>
+        <div className="flex flex-col items-center text-center lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 md:items-start md:text-left ">
+          <h1
+            className="mb-4 overflow-hidden text-lg font-medium text-gray-900 border-black opacity-0 title-font sm:text-4xl sm:leading-relaxed whitespace-nowrap animate-typing"
+            style={{ animationDelay: "1s" }}
+          >
+            꼼꼼하고 차분하게 생각하는 것을 목표로 하는
+          </h1>
+          <h1
+            className="mb-4 overflow-hidden text-lg font-medium text-gray-900 border-black opacity-0 title-font sm:text-4xl sm:leading-relaxed whitespace-nowrap animate-typingSecond "
+            style={{ animationDelay: "4.5s" }}
+          >
+            <span className="font-bold">FrontEnd 개발자 채다희입니다.</span>
+            <span className="animate-blink">|</span>
+          </h1>
+
           <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            transition={{ duration: 2 }}
+            initial={{ opacity: 0, y: 5 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: 5 }}
+            transition={{ duration: 2, delay: 7 }}
           >
             <p className="mb-4 leading-relaxed">
               설계부터 깊이 생각하고,
@@ -66,7 +66,13 @@ export default function AboutMe() {
               </span>
             </p>
           </motion.div>
-          <div className="flex flex-col flex-wrap items-center justify-center md:flex-row md:justify-between">
+          <motion.div
+            className="flex flex-col flex-wrap items-center justify-center md:flex-row md:justify-between"
+            initial={{ opacity: 0, y: 5 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: 5 }}
+            transition={{ duration: 2, delay: 7.5 }}
+          >
             {["cdh010126r@gmail.com", "GITHUB", "BLOG", "NOTION"].map(
               (text, index) => (
                 <motion.button
@@ -88,14 +94,25 @@ export default function AboutMe() {
                 </motion.button>
               )
             )}
-          </div>
-          <div className="w-full mt-3 text-lg text-left">
+          </motion.div>
+          <motion.div
+            className="left-0 flex items-start justify-between w-2/3 mt-3 text-lg "
+            initial={{ opacity: 0, y: 5 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: 5 }}
+            transition={{ duration: 2, delay: 8 }}
+          >
+            <p>
+              | 2001.01.26
+              <br />
+              | Address. 경기도 부천시 <br />
+            </p>
             <p>
               | Univ. 가톨릭대학교(2019.03-2025.02) <br />
               | Major. 미디어기술콘텐츠학과 <br />| Plural Major.
               컴퓨터정보공학부
             </p>
-          </div>
+          </motion.div>
         </div>
       </div>
     </section>
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,11 +10,32 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        bg: '#B0AFA6',
-        light: '#9E9D92',
-        dark: '#78776A',
-        textLight: '#4A5568', // text-gray-600
-        textDark: '#1F2937',  // text-gray-900
+        bg: "#B0AFA6",
+        light: "#9E9D92",
+        dark: "#78776A",
+        textLight: "#4A5568", // text-gray-600
+        textDark: "#1F2937", // text-gray-900
+      },
+      animation: {
+        typing: "fadeIn 1s forwards 1s , typing 4s steps(40, end) 1s forwards",
+        typingSecond:
+          "fadeIn 1s forwards 5s, typing 4s steps(40, end) 5s forwards",
+        blink: "blink 0.75s step-end infinite",
+      },
+      keyframes: {
+        typing: {
+          "0%": { width: "0%" },
+          "100%": { width: "100%" },
+        },
+        fadeIn: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        },
+        blink: {
+          "0%": { opacity: 1 },
+          "50%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        },
       },
     },
   },
