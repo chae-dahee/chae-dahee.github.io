55c2ec55288bdd14fc3b3914963107e448014ebd
“chae-dahee” <cdh010126r@gmail.com>
2024-09-10 19:02:39 +0900

    feat: 활동이력 내용 작성

--- src/components/home/activity.js
+++ src/components/home/activity.js
@@ -30,13 +30,13 @@ export default function Activity() {
               </div>
               <div className="flex-grow mt-6 md:pl-6 md:mt-0">
                 <h2 className="mb-1 text-xl font-medium text-gray-900 title-font">
-                  대학생 IT 연합 동아리 코테이토 8기~ (2023.09 - ing)
+                  대학생 IT 연합 동아리 코테이토 8기 (23.09 - 현재)
                 </h2>
                 <p className="leading-relaxed">
-                  프로젝트, 스터디, 해커톤, 협업, CS 교육자, 새로운 IT기술 공유
-                  의 다양한 활동에 적극참여했습니다. 타분야와 협업하는 방식과
-                  프론트엔드 구현 기술을 코드리뷰하며 학습했습니다. 프로젝트를
-                  통해 실전에서 적용하려 노력하였습니다.
+                  프로젝트, 스터디, 해커톤, 협업, CS 교육자, 최신 IT 기술 공유
+                  등 다양한 활동에 적극 참여하였습니다. 타 분야와 협업하는
+                  방식과 프론트엔드 구현 기술을 코드 리뷰를 통해 학습하였으며,
+                  프로젝트를 통해 실전에서 적용하려 노력하였습니다.
                 </p>
               </div>
             </div>
@@ -59,11 +59,47 @@ export default function Activity() {
               <div className="flex-grow mt-6 md:pl-6 md:mt-0">
                 <h2 className="mb-1 text-xl font-medium text-gray-900 title-font">
                   Programmers DevCource 타입스크립트로 함께하는 웹 풀 사이클
-                  개발(React, Node.js)
+                  개발 (24.04 ~ 24.10)
                 </h2>
                 <p className="leading-relaxed">
-                  VHS cornhole pop-up, try-hard 8-bit iceland helvetica. Kinfolk
-                  bespoke try-hard cliche palo santo offal.
+                  React와 Node.js를 활용하여 웹 애플리케이션의 설계부터 배포까지
+                  경험하였습니다. 현업 멘토에게 실무 트렌드 기술을 배우며 새로운
+                  기술을 적극적으로 학습하였고, 프론트엔드, 백엔드, 데브옵스
+                  경험을 통해 프로젝트의 전체적인 흐름을 깊이 이해하게
+                  되었습니다. 요구사항을 기반으로 꼼꼼한 설계 및 구축 프로세스를
+                  익혔으며, 개발, 테스트, 운영, 유지보수에 대한 이해도와 실력을
+                  갖추게 되었습니다.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="relative flex pt-10 pb-20 mx-auto md:items-center md:w-2/3">
+            <div className="absolute inset-0 flex items-center justify-center w-6 h-full left-4">
+              <div className="w-1 h-full bg-gray-200 pointer-events-none"></div>
+            </div>
+            <div className="relative z-10 inline-flex items-center justify-center flex-shrink-0 mt-10 text-sm font-medium text-white rounded-full h-14 w-14 bg-dark md:mt-0 title-font">
+              2023
+            </div>
+            <div className="flex flex-col items-start flex-grow pl-6 md:pl-8 md:items-center md:flex-row">
+              <div className="inline-flex items-center justify-center flex-shrink-0 w-24 h-24rounded-full">
+                <Image
+                  className="rounded-full"
+                  src={`${prefix}/리테일테크코딩교실.png`}
+                  alt="리테일테크코딩교실"
+                  width={100}
+                  height={100}
+                />
+              </div>
+              <div className="flex-grow mt-6 md:pl-6 md:mt-0">
+                <h2 className="mb-1 text-xl font-medium text-gray-900 title-font">
+                  신세계아이앤씨와 함께하는 리테일테크 코딩교실 6기 (23.09 -
+                  24.02)
+                </h2>
+                <p className="leading-relaxed">
+                  IT 인재 육성을 위해 서울 덕수중, 예천군 가족센터에서 코딩
+                  교육을 실시하였습니다. 파이썬 및 아두이노 PBL 실습과정과
+                  자율주제로 교육을 진행하였으며, 활동 우수 멘토로
+                  선정되었습니다.
                 </p>
               </div>
             </div>
@@ -72,61 +108,56 @@ export default function Activity() {
             <div className="absolute inset-0 flex items-center justify-center w-6 h-full left-4">
               <div className="w-1 h-full bg-gray-200 pointer-events-none"></div>
             </div>
-            <div className="relative z-10 inline-flex items-center justify-center flex-shrink-0 mt-10 text-sm font-medium text-white rounded-full h-14 w-14 bg-dark md:mt-0 title-font">
-              2023
-            </div>
+            <div className="relative z-10 inline-flex items-center justify-center flex-shrink-0 mt-10 text-sm font-medium rounded-full h-14 w-14 md:mt-0 title-font"></div>
             <div className="flex flex-col items-start flex-grow pl-6 md:pl-8 md:items-center md:flex-row">
-              <div className="inline-flex items-center justify-center flex-shrink-0 w-24 h-24 text-purple-500 bg-purple-100 rounded-full">
-                <svg
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  className="w-12 h-12"
-                  viewBox="0 0 24 24"
-                >
-                  <circle cx="12" cy="5" r="3"></circle>
-                  <path d="M12 22V8M5 12H2a10 10 0 0020 0h-3"></path>
-                </svg>
+              <div className="inline-flex items-center justify-center flex-shrink-0 w-24 h-24 rounded-full">
+                <Image
+                  className="rounded-full"
+                  src={`${prefix}/umc.webp`}
+                  alt="programmers"
+                  width={100}
+                  height={100}
+                />
               </div>
               <div className="flex-grow mt-6 md:pl-6 md:mt-0">
                 <h2 className="mb-1 text-xl font-medium text-gray-900 title-font">
-                  The 400 Blows
+                  대학생 개발 연합 동아리 University MakeUs Challenge (23.03 ~
+                  23.08)
                 </h2>
                 <p className="leading-relaxed">
-                  VHS cornhole pop-up, try-hard 8-bit iceland helvetica. Kinfolk
-                  bespoke try-hard cliche palo santo offal.
+                  처음 개발을 접하며 클론 코딩과 기획에 따른 개발을
+                  진행하였습니다. 별도로 리액트 스터디에 참여해 기초를 쌓았으며,
+                  기술 구현 시 파트 팀원과 논의하고 소통 후 개발하는 협업 방식을
+                  이해하게 되었습니다.
                 </p>
               </div>
             </div>
           </div>
-          <div className="relative flex pb-10 mx-auto md:items-center md:w-2/3">
+          <div className="relative flex pt-10 pb-20 mx-auto md:items-center md:w-2/3">
             <div className="absolute inset-0 flex items-center justify-center w-6 h-full left-4">
               <div className="w-1 h-full bg-gray-200 pointer-events-none"></div>
             </div>
-            <div className="relative z-10 inline-flex items-center justify-center flex-shrink-0 mt-10 text-sm font-medium rounded-full h-14 w-14 md:mt-0 title-font"></div>
+            <div className="relative z-10 inline-flex items-center justify-center flex-shrink-0 mt-10 text-sm font-medium text-white rounded-full h-14 w-14 bg-dark md:mt-0 title-font">
+              2022
+            </div>
             <div className="flex flex-col items-start flex-grow pl-6 md:pl-8 md:items-center md:flex-row">
-              <div className="inline-flex items-center justify-center flex-shrink-0 w-24 h-24 text-purple-500 bg-purple-100 rounded-full">
-                <svg
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  className="w-12 h-12"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
-                </svg>
+              <div className="inline-flex items-center justify-center flex-shrink-0 w-24 h-24rounded-full">
+                <Image
+                  className="rounded-full"
+                  src={`${prefix}/unity.jpg`}
+                  alt="리테일테크코딩교실"
+                  width={100}
+                  height={100}
+                />
               </div>
               <div className="flex-grow mt-6 md:pl-6 md:mt-0">
                 <h2 className="mb-1 text-xl font-medium text-gray-900 title-font">
-                  The Catalyzer
+                  게임 관련 경험 ( - 22)
                 </h2>
                 <p className="leading-relaxed">
-                  VHS cornhole pop-up, try-hard 8-bit iceland helvetica. Kinfolk
-                  bespoke try-hard cliche palo santo offal.
+                  게임 및 미디어 콘텐츠 제작 대회에서 모바일 게임 제작으로
+                  우수상을 수상하였고, 아동 게임 개발 인턴으로 9개월간
+                  활동하였습니다.
                 </p>
               </div>
             </div>
